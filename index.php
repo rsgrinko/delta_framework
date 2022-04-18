ed22916fa768475f6a072d177eebb50064c14b93
rsgrinko <rsgrinko@gmail.com>
2022-04-18 12:25:15 +0300

    fix name

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-    //test file
+    /**
+     * test inde file
+     */
+
     use Delta\ExternalServices\Telegram;
-require_once $_SERVER['DOCUMENT_ROOT']. '/core/lib/Delta/ExternalServices/Telegram.php';
-Telegram::init('5232660453:AAGfMWu6EcRfBGSSURJsEEvGPmAqhCyzYHU', './');
-Telegram::execute(412790359, 'Hello from test site');
+
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/core/lib/Delta/ExternalServices/Telegram.class.php';
+    Telegram::init('5232660453:AAGfMWu6EcRfBGSSURJsEEvGPmAqhCyzYHU', './');
+    Telegram::execute(412790359, 'Hello from test site');
