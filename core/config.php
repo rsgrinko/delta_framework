586e08383c7bdbaeb6f45b609f098dfc53bc0900
Ferhof <alexander.kiriltsev@gmail.com>
2022-04-19 19:40:03 +0300

    Типо рефакторинг

--- core/config.php
+++ core/config.php
@@ -1,57 +1,45 @@
 <?php
 
-    /**
-     * Версия сервиса
-     */
-    const VERSION = '1.0.0';
+/**
+ * Версия сервиса
+ */
+const VERSION = '1.0.0';
 
-    /**
-     * Путь до корня проекта
-     */
-    define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);
+/**
+ * Путь до корня проекта
+ */
+define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);
 
-    /**
-     * Путь до директории с логами
-     */
-    define('LOG_PATH', $_SERVER['DOCUMENT_ROOT'] . '/core/log');
+/**
+ * Путь до директории с логами
+ */
+define('LOG_PATH', $_SERVER['DOCUMENT_ROOT'] . '/core/log');
 
-    /**
-     * Префикс таблиц в БД
-     */
-    const TABLE_PREFIX = 'd_';
+/**
+ * Префикс таблиц в БД
+ */
+const TABLE_PREFIX = 'd_';
 
-    /**
-     * Сервер БД
-     */
-    const DB_HOST = 'localhost';
+/**
+ * БД
+ */
+const DB_HOST = 'localhost';
+const DB_USER = 'delta_core';
+const DB_PASSWORD = 'dickpick';
+const DB_NAME = 'delta_core';
 
-    /**
-     * Имя пользователя БД
-     */
-    const DB_USER = 'delta_core';
+/**
+ * Время, в течении которого считаем пользователя онлайн, сек.
+ */
+const USER_ONLINE_TIME = 60 * 5;
 
-    /**
-     * Пароль пользователя БД
-     */
-    const DB_PASSWORD = 'dickpick';
+/**
+ * Время жизни кэша
+ */
+const CACHE_TTL = 3600;
 
-    /**
-     * Название базы
-     */
-    const DB_NAME = 'delta_core';
-
-    /**
-     * Время, в течении которого считаем пользователя онлайн, сек.
-     */
-    const USER_ONLINE_TIME = 60 * 5;
-
-    /**
-     * Время жизни кэша
-     */
-    const CACHE_TTL = 3600;
-
-    /**
-     * Код значения Да
-     */
-    const CODE_VALUE_Y = 'Y';
+/**
+ * Код значения Да
+ */
+const CODE_VALUE_Y = 'Y';
 
