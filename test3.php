6960cb17b131960c8e6f14d2c694ab1f4ecf1c10
rsgrinko <rsgrinko@gmail.com>
2023-03-02 15:15:25 +0300

    [*] воркермен

--- test3.php
+++ test3.php
@@ -5,3 +5,12 @@
     require_once $_SERVER['DOCUMENT_ROOT'] . '/core/bootstrap.php';
 
     $DB = DB::getInstance();
+
+    use Workerman\Worker;
+
+    $worker = new Worker('websocket://0.0.0.0:8282');
+    $worker->onMessage = function($connection, $data)
+    {
+        $connection->send($data);
+    };
+    Worker::runAll();
