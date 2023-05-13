dcd717b6ba483550c47bbc254e0e563dbe4dab60
rsgrinko <rsgrinko@gmail.com>
2023-05-14 01:16:36 +0300

    [*] поправил уровень вывода ошибок

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -25,8 +25,8 @@
     use Core\CoreException;
     use Core\Models\Router;
 
-    //error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
-    error_reporting(E_ALL);
+    error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+    //error_reporting(E_ALL);
 
     if (session_status() == PHP_SESSION_NONE) {
         session_start();
